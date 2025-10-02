66653ffdbd9a49434fb47f9ab5f6122442e94bb2
braincandydan <graphics@inter-mtn.com>
2025-10-02 14:27:00 -0700

    Remove test-env.js file

--- pages/api/test-env.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    hasClientId: !!process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-    hasToken: !!process.env.TINA_TOKEN,
-    clientIdLength: process.env.NEXT_PUBLIC_TINA_CLIENT_ID?.length || 0,
-    tokenLength: process.env.TINA_TOKEN?.length || 0,
-  })
-}
